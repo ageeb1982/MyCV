97dc2cf2eacaab61dd5e4c5bdbb636c8146c0275
Mohamed Khidir Ageeb <ageeb1982@gmail.com>
2021-12-11 15:27:47 +0300

    update 2021_12_11_15h_25m

--- js/script.js
+++ js/script.js
@@ -1,14 +1,33 @@
+let minix = document.getElementsByClassName("minix");
 let lnks = document.getElementsByClassName("nav-link");
- var linkClick = function () {
+var linkClick = function () {
     for (var i = 0; i < lnks.length; i++) {
         lnks[i].classList.remove('active');
     }
-    console.log("this=>", this);
+
+
+
     this.classList.add('active');
 
 };
 
-for (var i = 0; i < lnks.length; i++) {
+for (let i = 0; i < lnks.length; i++) {
     lnks[i].addEventListener('click', linkClick, false);
 
- }
+}
+
+var miniClick = function () {
+    for (var i = 0; i < minix.length; i++) {
+        minix[i].classList.remove('active');
+    }
+
+
+
+    this.classList.add('active');
+
+};
+
+for (let i = 0; i < minix.length; i++) {
+    minix[i].addEventListener('click', miniClick, false);
+
+}
